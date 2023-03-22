35c2cd758d0c7be2d4494dff27d7029c9be39b28
Ustinova Ekaterina <galateya2406@yandex.ru>
2023-03-22 12:47:07 +0400

    Add fill

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 const canvas = document.getElementById('jsCanvas');
-canvas.height = 700;
-canvas.width = 700;
+
+const CANVAS_SIZE = 700;
+
+canvas.height = CANVAS_SIZE;
+canvas.width = CANVAS_SIZE;
 
 let painting = false;
+let filling = false;
 
 if (canvas) {
     canvas.addEventListener('mousedown', onMouseDown);
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+    canvas.addEventListener('click', handleCanvasClick);
+    canvas.addEventListener('contextmenu', handleCM);
 }
 
 function onMouseDown (event) {
@@ -24,6 +30,9 @@ function stopPainting (event) {
 }
 
 const ctx = canvas.getContext('2d');
+
+ctx.fillStyle = 'white';
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 
 ctx.lineWidth = 2.5;
 ctx.strokeStyle = 'black';
@@ -47,4 +56,56 @@ Array.from(colors).forEach(color => color.addEventListener('click', handleColorC
 function handleColorClick (event) {
     let color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+}
+
+const range = document.getElementById('jsRange');
+
+if (range) {
+    range.addEventListener('input', handleRangeChange);
+}
+
+function handleRangeChange (event) {
+    const rangeValue = event.target.value;
+    ctx.lineWidth = rangeValue;
+}
+
+const mode = document.getElementById('jsMode');
+
+if (mode) {
+    mode.addEventListener('click', handleModeClick);
+}
+
+function handleModeClick (event) {
+    if (filling) {
+        filling = false;
+        mode.innerText = 'Заливка';
+    } else {
+        filling = true;
+        mode.innerText = 'Рисование';
+    }
+}
+
+function handleCanvasClick (event) {
+    if (filling) {
+        ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+    }
+}
+
+const saveBtn = document.getElementById('jsSave');
+
+function handleCM (event) {
+    event.preventDefault();
+}
+
+if (saveBtn) {
+    saveBtn.addEventListener('click', handleSaveClick);
+}
+
+function handleSaveClick (event) {
+    const image = canvas.toDataURL();
+    const link = document.createElement('a');
+    link.href = image;
+    link.download = 'PaintJS [Export]';
+    link.click();
 }
